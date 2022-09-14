3ad04e2f1ef8484aaa1465f5995752411477dfa2
Benardboye <adeboyesamueladeyemi@yahoo.com>
2022-09-14 14:34:07 +0100

    challenge 3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,14 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+ function binaryReversal(value) {
+    let intToBinary = parseInt(value).toString(2);
+    let padStr = intToBinary.padStart(8,0);
+    let reverseStr = padStr.split('').reverse().join('')
+    let result = parseInt(reverseStr, 2);
+
+    return `${result}`
+}
 
 module.exports = binaryReversal;
+
